4a0871b5c7012d87c5037478147961a78fdf68d2
Casey O'Hara <casey.ohara@imulus.com>
2012-11-07 20:01:57 -0700

    Add preprocessor lookup on projects

--- lib/allen/project.rb
+++ lib/allen/project.rb
@@ -12,5 +12,23 @@ module Allen
       end
       @settings.configure(block) if block
     end
+
+    def css_preprocessor
+      if settings.css_preprocessor == :coyote
+        Allen::Preprocessors::Coyote
+      elsif settings.css_preprocessor == :sass
+        Allen::Preprocessors::Sass
+      else
+        raise StandardError, "unknown CSS preprocessor: #{settings.css_preprocessor}"
+      end
+    end
+
+    def js_preprocessor
+      if settings.js_preprocessor == :coyote
+        Allen::Preprocessors::Coyote
+      else
+        raise StandardError, "unknown JS preprocessor: #{settings.js_preprocessor}"
+      end
+    end
   end
 end
--- spec/lib/allen/project_spec.rb
+++ spec/lib/allen/project_spec.rb
@@ -1,32 +1,78 @@
 require 'allen'
 require 'allen/project'
 
-describe Allen::Project do
-
-  it "has good defaults" do
-    project = Allen::Project.new
-    project.name.should == "Umbraco"
-  end
-
-  it "makes its name also available in its settings" do
-    project = Allen::Project.new("Blog")
-    project.name.should == "Blog"
-    project.settings.name.should == "Blog"
-  end
-
-  it "derives its settings" do
-    Allen.settings.configure do
-      client "Ron Swanson"
-      name "Global Name"
-      cache true
+module Allen
+  module Preprocessors
+    class Coyote
     end
 
-    project = Allen::Project.new "MustacheProject"
+    class Sass
+    end
+  end
+end
 
-    Allen.settings.name.should == "Global Name"
-    project.settings.name.should == "MustacheProject"
-    project.settings.cache.should == true
+describe Allen::Project do
+  describe "settings" do
+    it "has good defaults" do
+      project = Allen::Project.new
+      project.name.should == "Umbraco"
+    end
+
+    it "makes its name also available in its settings" do
+      project = Allen::Project.new("Blog")
+      project.name.should == "Blog"
+      project.settings.name.should == "Blog"
+    end
+
+    it "derives its settings" do
+      Allen.settings.configure do
+        client "Ron Swanson"
+        name "Global Name"
+        cache true
+      end
+
+      project = Allen::Project.new "MustacheProject"
+
+      Allen.settings.name.should == "Global Name"
+      project.settings.name.should == "MustacheProject"
+      project.settings.cache.should == true
+    end
   end
 
+  describe "preprocessors" do
+    it "knows its css preprocessor" do
+      project = Allen::Project.new
+      project.settings.css_preprocessor :coyote
+      project.css_preprocessor.should == Allen::Preprocessors::Coyote
+    end
+
+    it "knows about sass" do
+      project = Allen::Project.new
+      project.settings.css_preprocessor :sass
+      project.css_preprocessor.should == Allen::Preprocessors::Sass
+    end
+
+    it "raises when it doesn't know the css preprocessor" do
+      project = Allen::Project.new
+      project.settings.css_preprocessor :pawikwkasdf
+      expect {
+        project.css_preprocessor
+      }.to raise_error StandardError, /unknown CSS preprocessor: pawikwkasdf/
+    end
+
+    it "knows its js preprocessor" do
+      project = Allen::Project.new
+      project.settings.js_preprocessor :coyote
+      project.js_preprocessor.should == Allen::Preprocessors::Coyote
+    end
+
+    it "raises when it doesn't know the js preprocessor" do
+      project = Allen::Project.new
+      project.settings.js_preprocessor :fsdfasdfadsf
+      expect {
+        project.js_preprocessor
+      }.to raise_error StandardError, /unknown JS preprocessor: fsdfasdfadsf/
+    end
+  end
 end
 
