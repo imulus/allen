6bfeb6bace5cf1f69070019caa237d49d8cd82e7
Casey O'Hara <casey.ohara@imulus.com>
2013-03-08 11:30:45 -0700

    Set banshee as the default preprocessor

--- lib/allen/settings.rb
+++ lib/allen/settings.rb
@@ -10,10 +10,10 @@ module Allen
         client           "Client"
         css_input        "assets/stylesheets/app/application.less"
         css_output       "css/application.css"
-        css_preprocessor :coyote
+        css_preprocessor :banshee
         js_input         "assets/javascripts/app/application.coffee"
         js_output        "js/application.js"
-        js_preprocessor  :coyote
+        js_preprocessor  :banshee
         root_dir         { pwd }
         src_dir          { "#{root_dir}/src" }
         solution         { "#{src_dir}/#{client}.sln" }
