8edf356e0153d684cbc173f4684a0add43b712a6
Casey O'Hara <casey.ohara@imulus.com>
2013-04-22 16:04:28 -0600

    Combine commit-hash and version plain text files into a single meta.json file

---
+++ lib/allen/meta_data.rb
@@ -0,0 +1,44 @@
+require 'grit'
+
+module Allen
+  class MetaData
+    attr_reader :root_dir, :webroot
+
+    def initialize(settings)
+      @root_dir = settings.root_dir
+      @webroot = settings.webroot
+    end
+
+    def save!
+      file = File.open("#{@webroot}/meta.json","w")
+      file.print(self.to_json)
+      file.close
+    end
+
+    def to_json
+      { :build => build,
+        :commit => commit }.to_json
+    end
+
+    def build
+      { :date => Time.now.to_s }
+    end
+
+    def commit
+      repo = Grit::Repo.new(@root_dir)
+      commit = repo.commits.first || NullCommit.new
+
+      { :id => commit.id,
+        :date => commit.committed_date,
+        :author => commit.author,
+        :message => commit.message }
+    end
+
+    class NullCommit
+      def method_missing(*args)
+        "xxx"
+      end
+    end
+
+  end
+end
